ed90dd9316e7a0d24b9d106a44ffeffc7f1fc69a
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-29 00:30:54 +0100

    initial commit

---
+++ dist/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+const PORT = 3009;
+const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf8'));
+app.get('/', (req, res) => {
+    res.send('job site api');
+});
+app.get('/jobs', (req, res) => {
+    res.json(jobs);
+});
+app.listen(PORT, () => {
+    console.log(`listening on port http://localhost:${PORT}`);
+});
+//# sourceMappingURL=server.js.map
